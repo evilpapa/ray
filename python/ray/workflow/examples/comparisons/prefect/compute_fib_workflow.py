82989119430713471f5af2797b996329f5f4f29d
can <can@anyscale.com>
2024-04-26 13:08:40 +0000

    [core] fix workflow tests
    Signed-off-by: can <can@anyscale.com>

--- python/ray/workflow/examples/comparisons/prefect/compute_fib_workflow.py
+++ python/ray/workflow/examples/comparisons/prefect/compute_fib_workflow.py
@@ -1,6 +1,5 @@
 import ray
 from ray import workflow
-import requests
 
 
 def fibonacci(n):
@@ -13,14 +12,7 @@ def fibonacci(n):
 
 @ray.remote
 def compute_large_fib(M: int, n: int = 1, fib: int = 1):
-    try:
-        next_fib = requests.post(
-            "https://nemo.api.stdlib.com/fibonacci@0.0.1/", data={"nth": n}
-        ).json()
-        assert isinstance(next_fib, int)
-    except AssertionError:
-        # TODO(suquark): The web service would fail sometimes. This is a workaround.
-        next_fib = fibonacci(n)
+    next_fib = fibonacci(n)
     if next_fib > M:
         return fib
     else:
